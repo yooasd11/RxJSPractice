625f108d04bf489d30af8e1c441c92c9e4088055
jaeyoon <jaeyoon@bluehole.net>
2019-07-24 22:15:14 +0900

    Auto complete 작업

--- autoComplete/main.js
+++ autoComplete/main.js
@@ -1,13 +1,37 @@
-const { fromEvent } = rxjs;
-const { map, mergeMap, debounceTime, filter, distinctUntilChanged } = rxjs.operators;
+const { fromEvent, Subject } = rxjs;
+const { finalize, retry, partition, map, mergeMap, debounceTime, filter, distinctUntilChanged, tap, switchMap, catchError } = rxjs.operators;
 const { ajax } = rxjs.ajax;
 
-const users$ = fromEvent(document.getElementById('search'), 'keyup').pipe(
+const subject = new Subject();
+
+const $loading = document.getElementById('loading');
+
+const showLoading = () => {
+	$loading.style.display = 'block';
+};
+
+const hideLoading = () => {
+	$loading.style.display = 'none';
+};
+
+const keyup$ = fromEvent(document.getElementById('search'), 'keyup').pipe(
 	debounceTime(300),
 	map(e => e.target.value),
 	distinctUntilChanged(),
-	filter(query => query.trim().length > 0),
-	mergeMap(query => ajax.getJSON(`https://api.github.com/search/users?q=${query}`)),
+);
+
+let [user$, reset$] = subject.pipe(
+	partition(q => q.trim().length > 0),
+);
+
+keyup$.subscribe(subject);
+
+users$ = user$.pipe(
+	tap(showLoading),
+	switchMap(q => ajax.getJSON(`https://api.github.com/search/users?q=${q}`)),
+	tap(hideLoading),
+	retry(2),
+	finalize(hideLoading),
 );
 
 const $layer = document.getElementById('suggestLayer');
@@ -23,4 +47,14 @@ const drawLayer = items => {
 	`).join('');
 };
 
-users$.subscribe(user => drawLayer(user.items));
+users$.subscribe({
+	next: user => drawLayer(user.items),
+	error: e => {
+		console.log('Something went wrong : ', e),
+		alert(e.message);
+	}
+});
+
+reset$.pipe(
+	tap(v => $layer.innerHTML = ''),
+).subscribe();
